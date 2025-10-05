92308e293b03241b2e72417eafc824aeb3759b16
Abdus Samad <abdussamadse@gmail.com>
2025-10-05 15:31:47 +0600

    Login page created

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,16 @@
+import { Link } from "expo-router";
 import React from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-2xl font-rubik-extrabold text-blue-500">
+      <Text className="text-2xl font-rubik-extrabold text-blue-500 mb-4">
         Welcome to Nativewind!
       </Text>
+      <Link href="/sign-in" className="text-blue-500 text-lg">
+        Sign in
+      </Link>
     </View>
   );
 }
