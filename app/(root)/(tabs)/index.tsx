8186c577d774e633c117dbba40953787a456e275
Abdus Samad <abdussamadse@gmail.com>
2025-10-05 20:04:19 +0600

    profile page added

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,4 +1,3 @@
-import { Link } from "expo-router";
 import React from "react";
 import { Text, View } from "react-native";
 
@@ -8,9 +7,6 @@ export default function Index() {
       <Text className="text-2xl font-rubik-extrabold text-blue-500 mb-4">
         Welcome to Nativewind!
       </Text>
-      <Link href="/sign-in" className="text-blue-500 text-lg">
-        Sign in
-      </Link>
     </View>
   );
 }
